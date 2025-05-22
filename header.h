91343c5d729fa618c38ae8d40389c5508bd5b7aa
Farel <alexanderkimf@gmail.com>
2025-05-23 00:50:38 +0700

    contoh 2d

--- header.h
+++ header.h
@@ -5,3 +5,27 @@ typedef int **matrix; //define matrix
 typedef int *intArray; //harus di Calloc yak!!
 
 matrix createMatrix();
+
+/* CONTOH AJA
+
+int main(){
+    matrix mtx = createMatrix();
+
+    for(int i = 0; i < 5; i++)for(int j = 0; j <5 ; j++)mtx[i][j] = (i * 5) + j;
+    // Masukin Nilai 1 - 25
+
+
+
+    printf("success\n");
+    for(int i = 0; i < 5; i++) {
+        for(int j = 0; j < 5 ; j++){
+            printf("[%02d]", mtx[i][j]);
+        }
+        
+        printf("\n");
+    }
+    free(mtx);
+    return 0;
+}
+
+*/
