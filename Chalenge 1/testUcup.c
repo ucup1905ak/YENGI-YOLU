853dcd3300849aa3679fe02dbb68a85577d91a65
Farel <alexanderkimf@gmail.com>
2025-05-31 19:40:49 +0700

    rapih

---
+++ Chalenge 1/testUcup.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "header.h"
+int main(){
+    bool authentication = false;
+    int percobaan = 3;
+    while(!authentication){
+        system("cls");
+        // loginDisplay(&percobaan, &authentication);
+    }
+    while(1) {
+
+        // printf("\nCaptha: ");generateChapta();
+        delay(1000);
+    }
+    return 0;
+}
