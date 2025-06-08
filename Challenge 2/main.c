7b719e18e5fcf4f5053019cd15d639f8e90bb124
Farel <alexanderkimf@gmail.com>
2025-06-08 14:51:42 +0700

    UPDATE 1.0 - REnder Engine

---
+++ Challenge 2/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+    printf("\n\t---== [ HUNGRY FISH ] ==---\n");
+    puts("\n\t[1] ");
+    puts("\n\t[2] ");
+    puts("\n\t[3] ");
+    return 0;
+}
