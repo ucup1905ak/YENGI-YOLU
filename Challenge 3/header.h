588e64f168aadd88fd809b5463e945d6f814d0de
Farel <alexanderkimf@gmail.com>
2025-06-15 19:46:25 +0700

    update

--- Challenge 3/header.h
+++ Challenge 3/header.h
@@ -41,7 +41,7 @@ void encryptPassword(char *dest, const char *src);
 int isStrongPassword(const char *pass);
 void inputPassword(char *dest);
 int isValidEmail(const char *email);
-void loginMenu(user users);
+void loginMenu(user * users);
 int isLoginFound(user u, string name, string pass);
 user* searchLoginData(UserList userList, string name, string pass);
 bool captcha();
