e5d7ef59541ad2771cd28de80862edc4b0f83cbc
Farel <alexanderkimf@gmail.com>
2025-06-21 08:24:34 +0700

    push

--- Final Yengi Yolu/src/ui.h
+++ Final Yengi Yolu/src/ui.h
@@ -1,1 +1,25 @@
 #include "logic.h"
+#include <stdio.h>
+#include <windows.h>
+#include <unistd.h>
+#include <string.h>
+
+#include <conio.h>
+#define NORMAL "\033[0m"
+
+//Forground
+#define RED "\033[31m"
+#define GREEN "\033[32m"
+#define YELLOW "\033[33m"
+#define BLUE "\033[34m"
+#define MAGENTA "\033[35m"
+#define CYAN "\033[36m"
+#define WHITE "\033[37m"
+//background
+#define BG_RED      "\033[41m"
+#define BG_GREEN    "\033[42m"
+#define BG_YELLOW   "\033[43m"
+#define BG_BLUE     "\033[44m"
+#define BG_MAGENTA  "\033[45m"
+#define BG_CYAN     "\033[46m"
+#define BG_WHITE    "\033[47m"
